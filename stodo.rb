f182730763f6f3514d86fbd05c3a926982a76f3e
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-01-04 18:25:54 +0900

    Remove all generated comments

--- stodo.rb
+++ stodo.rb
@@ -1,39 +1,17 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Stodo < Formula
   desc "Swifty command-line tool for todo list."
   homepage "https://pixyzehn.com"
-  url "https://github.com/pixyzehn/stodo/archive/0.0.3.tar.gz"
+  url "https://github.com/pixyzehn/stodo.git"
   version "0.0.3"
   sha256 "d570c723d653a47ae0bb68e31b86f889f5f5081b310e1a0e8baf243021bcdbf9"
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on 'carthage' => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test stodo`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system bin/"stodo"
   end
 end

428635fb755027a492ace5049a339c0a448369ff
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-01-05 09:41:21 +0900

    Bump version to 0.0.5

--- stodo.rb
+++ stodo.rb
@@ -2,9 +2,9 @@ class Stodo < Formula
   desc "Swifty command-line tool for todo list."
   homepage "https://pixyzehn.com"
   url "https://github.com/pixyzehn/stodo.git",
-      :tag => "0.0.4",
+      :tag => "0.0.5",
       :shallow => false
-  version "0.0.4"
+  version "0.0.5"
   sha256 "d570c723d653a47ae0bb68e31b86f889f5f5081b310e1a0e8baf243021bcdbf9"
 
   depends_on :xcode => ["8.0", :build]

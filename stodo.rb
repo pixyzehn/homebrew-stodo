34861b5c77d6c2ce11ce6534e32b4bb8df0bf8e0
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-05-03 17:36:50 +0900

    Bump version to 0.1.1

--- stodo.rb
+++ stodo.rb
@@ -2,17 +2,17 @@ class Stodo < Formula
   desc "Swifty command-line tool for todo list."
   homepage "https://pixyzehn.com"
   url "https://github.com/pixyzehn/stodo.git",
-      :tag => "0.1.0",
+      :tag => "0.1.1",
       :shallow => false
-  version "0.1.0"
+  version "0.1.1"
   sha256 "d570c723d653a47ae0bb68e31b86f889f5f5081b310e1a0e8baf243021bcdbf9"
 
   bottle do
     cellar :any
-    root_url "https://github.com/pixyzehn/stodo/releases/download/0.1.0"
+    root_url "https://github.com/pixyzehn/stodo/releases/download/0.1.1"
   end
 
-  depends_on :xcode => ["8.0", :build]
+  depends_on :xcode => ["8.3", :build]
   depends_on 'carthage' => :build
 
   def install
